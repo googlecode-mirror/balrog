4c2be4a38d906aaa30daaad22cde94bc39dd10b7
gringoperalta <gringoperalta@02d386ba-ac3a-11dd-ab18-15145061d018>
2009-07-25 00:48:08 +0000

    Base Model for v0.5

--- trunk/Model.class.php
+++ trunk/Model.class.php
@@ -3,12 +3,28 @@ require_once 'lib/Connection.class.php';
 abstract class Model
 {
 	private $result;
+	/**
+	 * Default Model constructor
+	 * 
+	 * @return 
+	 */
 	public function __construct(){
 		$this->result = NULL;
 	}
+	/**
+	 * Excecutes the given query
+	 * 
+	 * @param string $sql
+	 * @return 
+	 */
 	public function query($sql){
 		$this->result = Connection::getInstance()->query($sql);
 	}
+	/**
+	 * Fetches the next record from the current recordset
+	 * 
+	 * @return 
+	 */
 	public function next(){
 		return $this->result->fetch(PDO::FETCH_INTO, $this);
 	}
