37389bf089da75e28cee1bdb4375825a3bc32fa8
gringoperalta <gringoperalta@02d386ba-ac3a-11dd-ab18-15145061d018>
2010-03-12 17:20:36 +0000

    Improvements to data access layer.

--- Connection.class.php
+++ Connection.class.php
@@ -1,35 +1,41 @@
-<?php 
-class Connection {
-    private $host;
-    private $name;
-    private $user;
-    private $pass;
-    private static $instance = NULL;
-    /**
-     * The constructor is set to private so nobody can create a new instance of the class
-     */
-    private function __construct() {
-        $this->host = Settings::get('blrg:database/blrg:host');
-        $this->name = Settings::get('blrg:database/blrg:name');
-        $this->user = Settings::get('blrg:database/blrg:user');
-        $this->pass = Settings::get('blrg:database/blrg:pass');
-    }
-    /**
-     * Singleton method that requests a unique instance of the class
-     *
-     * @return object (PDO)
-     * @access public
-     */
-    public static function getInstance() {
-        if (!self::$instance) {
-            self::$instance = new PDO("mysql:host='localhost';dbname='animals'", 'username', 'password');
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        return self::$instance;
-    }
-    /**
-     * This magic method is set as private so nobody can clone the instance
-     */
-    private function __clone() {
-    }
+<?php
+class Connection extends PDO {
+	private $host;
+	private $name;
+	private $user;
+	private $pass;
+	private static $instance = NULL;
+	/**
+	 * The constructor is set to private so nobody can create a new instance of the class
+	 *
+	 * @todo DB Type should be configurable.
+	 */
+	public function __construct() {
+		$this->host = Settings::get('blrg:database/blrg:host');
+		$this->name = Settings::get('blrg:database/blrg:name');
+		$this->user = Settings::get('blrg:database/blrg:user');
+		$this->pass = Settings::get('blrg:database/blrg:pass');
+		parent::__construct("mysql:dbname=$this->name;host=$this->host", $this->user, $this->pass);
+	}
+	/**
+	 * Singleton method that requests a unique instance of the class
+	 *
+	 * @return object (PDO)
+	 * @access public
+	 */
+	public static function getInstance() {
+		if (!self::$instance instanceof self) {
+			try{
+				self::$instance = new self();
+				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} catch (PDOException $e) {
+				echo 'Connection failed: ' . $e->getMessage();
+			}
+		}
+		return self::$instance;
+	}
+	/**
+	 * This magic method is set as private so nobody can clone the instance
+	 */
+	private function __clone() {}
 }
